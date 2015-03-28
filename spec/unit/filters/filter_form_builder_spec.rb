5185fbc404f31d178037d4434ead57041542df65
Javier Julio <jjfutbol@gmail.com>
2015-03-27 22:54:34 -0400

    Major forms revamp
    * removed separator (can be done in CSS if necessary)
    * add “action” class on cancel button for consistent selecting
    * simplified form selectors and removed styles not having any effect as
    much as possible
    
    I can do a lot more but the code for generating forms is rather dynamic
    and hard to follow. I’ll need more time to give this a bigger revamp.
    Most important is I’d like to standardize some classes on components
    rather than targeting elements in specific areas. Keeps both our HTML
    and CSS simple and predictable.
    
    Conflicts:
    	app/assets/stylesheets/active_admin/_forms.scss
    	lib/active_admin/form_builder.rb

--- spec/unit/filters/filter_form_builder_spec.rb
+++ spec/unit/filters/filter_form_builder_spec.rb
@@ -179,9 +179,6 @@ RSpec.describe ActiveAdmin::Filters::ViewHelper do
     it "should generate a date greater than" do
       expect(body).to have_selector("input.datepicker[name='q[created_at_gteq_datetime]']")
     end
-    it "should generate a seperator" do
-      expect(body).to have_selector("span.seperator")
-    end
     it "should generate a date less than" do
       expect(body).to have_selector("input.datepicker[name='q[created_at_lteq_datetime]']")
     end

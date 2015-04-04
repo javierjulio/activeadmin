0af476f44a73299b83f9e6ac6bb6bf0a7a585e43
Javier Julio <jjfutbol@gmail.com>
2015-04-03 22:35:12 -0400

    Display date placeholder in date picker field, remove icon
    We should show placeholders where possible. Clearer to show format than
    to have the icon. Ideally we’d like to have SVG icons that can work in
    both CSS and in HTML (current Iconic solution only works in HTML). The
    icons look like they could be updated too (Iconic is a paid solution so
    we should look into an open source SVG one if necessary). Remove nbsp
    as its unnecessary.

--- lib/active_admin/inputs/filters/date_range_input.rb
+++ lib/active_admin/inputs/filters/date_range_input.rb
@@ -7,8 +7,8 @@ module ActiveAdmin
         def to_html
           input_wrapping do
             [ label_html,
-              builder.text_field(gt_input_name, input_html_options(gt_input_name, gt_input_placeholder)),
-              builder.text_field(lt_input_name, input_html_options(lt_input_name, lt_input_placeholder)),
+              builder.text_field(gt_input_name, input_html_options(gt_input_name)),
+              builder.text_field(lt_input_name, input_html_options(lt_input_name)),
             ].join("\n").html_safe
           end
         end

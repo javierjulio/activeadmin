96cfb0f1891dd469a469603452afe754d77fe0ee
Javier Julio <jjfutbol@gmail.com>
2015-04-14 21:46:41 -0400

    Update and configure panel styles
    We prefer to keep element class names specific so we have simple
    selectors. Update specs to account for changes. Remove sections mixin.
    We might bring it back for some methods but ideally we’d like to style
    forms using panels separately from form HTML although that could be
    significant work.

--- features/step_definitions/attributes_table_title_steps.rb
+++ features/step_definitions/attributes_table_title_steps.rb
@@ -1,9 +1,9 @@
 Then /^I should see the panel title "([^"]*)"$/ do |title|
-  expect(page).to have_css ".panel > h3", text: title
+  expect(page).to have_css ".panel-title", text: title
 end
 
 Then /^I should not see the panel title "([^"]*)"$/ do |title|
-  expect(page).to_not have_css ".panel > h3", text: title
+  expect(page).to_not have_css ".panel-title", text: title
 end
 
 Then /^I should see the attributes table$/ do

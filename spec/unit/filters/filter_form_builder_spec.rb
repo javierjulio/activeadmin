794908ad6d672ab0c8ce9b1afa34d75f31629e50
Javier Julio <jjfutbol@gmail.com>
2015-04-21 16:16:50 -0400

    Major change for global button and button theme classes
    This makes it much easier to apply button styles across the admin and
    allows users to specify their own themes too. For example, user
    provided action items now require that you set button class and theme
    class rather than just styling them all the same. You could apply
    different classes to each. Further this cuts down on our CSS and
    promotes better reusability for users by simply using class.
    
    Add button and button-primary class to Devise views
    
    Remove unnecessary position handling from dropdown JS
    
    Update modal dialog JS with explicit button classes

--- spec/unit/filters/filter_form_builder_spec.rb
+++ spec/unit/filters/filter_form_builder_spec.rb
@@ -46,7 +46,7 @@ RSpec.describe ActiveAdmin::Filters::ViewHelper do
     end
 
     it "should generate a clear filters link" do
-      expect(body).to have_selector("a.clear_filters_btn", text: "Clear Filters")
+      expect(body).to have_selector("a.button-default", text: "Clear Filters")
     end
 
     describe "label as proc" do

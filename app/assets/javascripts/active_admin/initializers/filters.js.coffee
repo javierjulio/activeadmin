46b98b8404b5dba78806800d51a87a44cd3fde35
Joel Clay <ra3don92@gmail.com>
2015-08-10 22:15:41 -0400

    fixing hound issues on _form.scss and _typography.scss

--- app/assets/javascripts/active_admin/initializers/filters.js.coffee
+++ app/assets/javascripts/active_admin/initializers/filters.js.coffee
@@ -6,10 +6,10 @@ $(document).on 'ready page:load turbolinks:load', ->
     window.location.search = (param for param in params when not param.match(regex)).join('&')
 
   # Filter form: don't send any inputs that are empty
-  $('.filter_form').submit ->
+  $('.filter-form').submit ->
     $(@).find(':input').filter(-> @value is '').prop 'disabled', true
 
   # Filter form: for filters that let you choose the query method from
   # a dropdown, apply that choice to the filter input field.
-  $('.filter_form_field.select_and_search select').change ->
+  $('.filter-form-field.select-and-search select').change ->
     $(@).siblings('input').prop name: "q[#{@value}]"
--- spec/unit/form_builder_spec.rb
+++ spec/unit/form_builder_spec.rb
@@ -472,22 +472,22 @@ describe ActiveAdmin::FormBuilder do
       end
 
       it "should add a link to remove new nested records" do
-        expect(body).to have_selector(".has_many_container > fieldset > ol > li > a.button.has_many_remove[href='#']", text: "Remove")
+        expect(body).to have_selector(".has-many-container > fieldset > ol > li > a.button.has_many_remove[href='#']", text: "Remove")
       end
 
       it "should add a link to add new nested records" do
-        expect(body).to have_selector(".has_many_container > a.button.has_many_add[href='#']", text: "Add New Post")
+        expect(body).to have_selector(".has-many-container > a.button.has_many_add[href='#']", text: "Add New Post")
       end
 
       it "should set an HTML-id valid placeholder" do
-        link = body.find('.has_many_container > a.button.has_many_add')
+        link = body.find('.has-many-container > a.button.has_many_add')
         expect(link[:'data-placeholder']).to match valid_html_id
       end
 
       describe "with namespaced model" do
         it "should set an HTML-id valid placeholder" do
           allow(Post).to receive(:name).and_return "ActiveAdmin::Post"
-          link = body.find('.has_many_container > a.button.has_many_add')
+          link = body.find('.has-many-container > a.button.has_many_add')
           expect(link[:'data-placeholder']).to match valid_html_id
         end
       end
@@ -591,7 +591,7 @@ describe ActiveAdmin::FormBuilder do
         end
 
         it "should wrap the destroy field in an li with class 'has_many_delete'" do
-          expect(body).to have_selector(".has_many_container > fieldset > ol > li.has_many_delete > input", count: 1)
+          expect(body).to have_selector(".has-many-container > fieldset > ol > li.has_many_delete > input", count: 1)
         end
       end
 
@@ -628,7 +628,7 @@ describe ActiveAdmin::FormBuilder do
         end
 
         it "shows the nested fields for unsaved records" do
-          expect(body).to have_selector("fieldset.inputs.has_many_fields")
+          expect(body).to have_selector("fieldset.inputs.has-many-fields")
         end
 
       end
@@ -645,7 +645,7 @@ describe ActiveAdmin::FormBuilder do
         end
 
         it "shows the nested fields for unsaved records" do
-          expect(body).to have_selector("fieldset.inputs.has_many_fields")
+          expect(body).to have_selector("fieldset.inputs.has-many-fields")
         end
 
       end
@@ -667,7 +667,7 @@ describe ActiveAdmin::FormBuilder do
         end
 
         it "shows the nested fields for saved and unsaved records" do
-          expect(body).to have_selector("fieldset.inputs.has_many_fields")
+          expect(body).to have_selector("fieldset.inputs.has-many-fields")
         end
       end
 
@@ -682,7 +682,7 @@ describe ActiveAdmin::FormBuilder do
         end
 
         it "defaults to 0" do
-          expect(body).to have_selector("div.has_many_container[data-sortable-start='0']")
+          expect(body).to have_selector("div.has-many-container[data-sortable-start='0']")
         end
       end
 
@@ -697,7 +697,7 @@ describe ActiveAdmin::FormBuilder do
         end
 
         it "sets the data attribute" do
-          expect(body).to have_selector("div.has_many_container[data-sortable-start='15']")
+          expect(body).to have_selector("div.has-many-container[data-sortable-start='15']")
         end
       end
     end
@@ -716,15 +716,15 @@ describe ActiveAdmin::FormBuilder do
         end
 
         it "should wrap the has_many fieldset in an li" do
-          expect(body).to have_selector("ol > li.has_many_container")
+          expect(body).to have_selector("ol > li.has-many-container")
         end
 
         it "should have a direct fieldset child" do
-          expect(body).to have_selector("li.has_many_container > fieldset")
+          expect(body).to have_selector("li.has-many-container > fieldset")
         end
 
         it "should not contain invalid li children" do
-          expect(body).not_to have_selector("div.has_many_container > li")
+          expect(body).not_to have_selector("div.has-many-container > li")
         end
       end
 
@@ -742,11 +742,11 @@ describe ActiveAdmin::FormBuilder do
         end
 
         it "should wrap the inner has_many fieldset in an ol > li" do
-          expect(body).to have_selector(".has_many_container ol > li.has_many_container > fieldset")
+          expect(body).to have_selector(".has-many-container ol > li.has-many-container > fieldset")
         end
 
         it "should not contain invalid li children" do
-          expect(body).not_to have_selector(".has_many_container div.has_many_container > li")
+          expect(body).not_to have_selector(".has-many-container div.has-many-container > li")
         end
       end
     end

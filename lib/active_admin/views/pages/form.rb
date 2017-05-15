aaf2210d3dfdf77c6a4f9efc425f56ac380e307e
Javier Julio <jjfutbol@gmail.com>
2017-05-15 10:44:26 -0400

    Fix create_another support
    No need to specify a custom block for the form actions in the form
    component. I thought I did at first to get the default rendering I
    wanted but this seems to be fine due to later changes. I learned a lot
    about ActiveAdmin as I spent time working on this as I was unfamiliar
    with its setup.
    
    Due to our drastic CSS changes most of the create_another styles are
    not necessary since we’ve simplified and removed unused styles.

--- lib/active_admin/views/pages/form.rb
+++ lib/active_admin/views/pages/form.rb
@@ -43,10 +43,7 @@ module ActiveAdmin
           ActiveAdmin::PagePresenter.new do |f|
             f.semantic_errors # show errors on :base by default
             f.inputs
-            f.actions do
-              f.action :submit, button_html: { class: "button button-primary" }
-              f.action :cancel, button_html: { class: "button button-default" }
-            end
+            f.actions
           end
         end
 

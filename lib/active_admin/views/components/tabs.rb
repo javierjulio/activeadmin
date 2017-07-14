400c3a2a555c1872c6b885c2270286cc17f1aedc
Javier Julio <jjfutbol@gmail.com>
2017-07-13 21:45:09 -0400

    Replace tabs jQuery UI implementation with bootstrap

--- lib/active_admin/views/components/tabs.rb
+++ lib/active_admin/views/components/tabs.rb
@@ -12,15 +12,21 @@ module ActiveAdmin
       def build(&block)
         @menu = ul(class: 'nav nav-tabs', role: "tablist")
         @tabs_content = div(class: 'tab-content')
+        @id_prefix = Time.current.to_i.to_s
+        @is_first_item = true
       end
 
       def build_menu_item(title, options, &block)
-        fragment = options.fetch(:id, title.parameterize)
-        li { link_to title, "##{fragment}" }
+        fragment = @id_prefix + options.fetch(:id, title.parameterize)
+        li { link_to title, "##{fragment}", data: { toggle: "tab" } }
       end
 
       def build_content_item(title, options, &block)
-        options = options.reverse_merge(id: title.parameterize)
+        fragment = @id_prefix + options.fetch(:id, title.parameterize)
+        classes = [ 'tab-pane' ]
+        classes << 'active' if @is_first_item
+        @is_first_item = false
+        options = options.reverse_merge(id: fragment, class: classes.join(' '))
         div(options, &block)
       end
     end

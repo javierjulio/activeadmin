javierjulio/activeadmin

commit 86e4a7d85776486b045dd96d2e349a9e178492d2

Group scopes by group name and render them in different lists, adding empty space between them

lib/active_admin/views/components/scopes.rb
@@ -13,16 +13,20 @@ module ActiveAdmin
       include ::ActiveAdmin::Helpers::Collection
 
       def default_class_name
-        "scopes table_tools_segmented_control"
+        "scopes"
       end
 
       def tag_name
-        'ul'
+        'div'
       end
 
       def build(scopes, options = {})
-        scopes.each do |scope|
-          build_scope(scope, options) if call_method_or_proc_on(self, scope.display_if_block)
+        scopes.group_by(&:group).each do |group, group_scopes|
+          ul class: "table_tools_segmented_control #{group_class(group)}" do
+            group_scopes.each do |scope|
+              build_scope(scope, options) if call_method_or_proc_on(self, scope.display_if_block)
+            end
+          end
         end
       end
 
@@ -60,6 +64,9 @@ module ActiveAdmin
         collection_size(scope_chain(scope, collection_before_scope))
       end
 
+      def group_class(group)
+        group.present? ? "scope-group-#{group}" : "scope-default-group"
+      end
     end
   end
 end

app/assets/stylesheets/active_admin/components/_scopes.scss
@@ -6,5 +6,8 @@
       font-size: 0.9em;
       line-height: 10px;
     }
+    &:first-child a {
+      margin-left: 10px;
+    }
   }
 }

commit ce2702c2bfbe0bb9950e98048b75f16c8012ba23

Fix scopes to align with batch actions button
This was broken in commit 86e4a7d85776486b045dd96d2e349a9e178492d2 as it removed the class name. Its necessary to keep the scopes inline with the batch actions button.

lib/active_admin/views/components/scopes.rb
@@ -13,7 +13,7 @@ module ActiveAdmin
       include ::ActiveAdmin::Helpers::Collection
 
       def default_class_name
-        "scopes"
+        "scopes table_tools_segmented_control"
       end
 
       def tag_name